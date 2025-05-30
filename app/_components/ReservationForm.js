d4100cfa429d8a9de7f1e82d85cf79e0f662c679
Gyenes Gergo <gergo.gyenes@gmail.com>
2025-05-30 14:18:52 +0200

    New reservation

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -1,10 +1,28 @@
 "use client";
 
+import { differenceInDays } from "date-fns";
 import { useReservation } from "./ReservationContext";
+import { createBooking } from "../_lib/actions";
+import SubmitButton from "./SubmitButton";
 
 function ReservationForm({ cabin, user }) {
-    const { range } = useReservation();
-    const { maxCapacity } = cabin;
+    const { range, resetRange } = useReservation();
+    const { maxCapacity, regularPrice, discount, id } = cabin;
+
+    const startDate = range.from;
+    const endDate = range.to;
+    const numNights = differenceInDays(endDate, startDate);
+    const cabinPrice = numNights * (regularPrice - discount);
+
+    const bookingData = {
+        startDate,
+        endDate,
+        numNights,
+        cabinPrice,
+        cabinId: id,
+    };
+
+    const createBookingWithData = createBooking.bind(null, bookingData);
 
     return (
         <div className="scale-[1.01]">
@@ -25,7 +43,14 @@ function ReservationForm({ cabin, user }) {
                 }
             </div>
 
-            <form className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col">
+            <form
+                // 1.st version action={createBookingWithData}
+                action={async (formData) => {
+                    await createBookingWithData(formData);
+                    resetRange();
+                }}
+                className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col"
+            >
                 <div className="space-y-2">
                     <label htmlFor="numGuests">How many guests?</label>
                     <select
@@ -65,9 +90,9 @@ function ReservationForm({ cabin, user }) {
                         Start by selecting dates
                     </p>
 
-                    <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
+                    <SubmitButton pendingLabel="Reserving...">
                         Reserve now
-                    </button>
+                    </SubmitButton>
                 </div>
             </form>
         </div>
